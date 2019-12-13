884acc8c96de8488af66b819bd49f56f2531f4df
zhu <l>
2019-12-13 10:24:33 +0800

    更新提交3

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,33 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  data:{
+    name:"首页",
+    age:18,
+    students:[
+      {id:110,name:"curry",age:30},
+      {id: 112, name:"cur33", age:32}
+    ],
+    counter:0
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  //增加数字
+  handleAdd(){
+    //1.错误 页面不会刷新
+   // console.log("点击了按钮")
+   //this.data.counter +=1
+
+   //2 this.setData()
+   this.setData({
+     counter:this.data.counter+1
+   })
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+//减少
+    handleReduce() {
+      this.setData({
+      counter:this.data.counter-1
+      })
   }
+
 })
